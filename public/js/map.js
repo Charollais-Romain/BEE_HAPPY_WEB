169f2f84214b9e5fc8bd3b60458549dd4ad39a2b
liloumorgan13-glitch <liloumorgan.13@gmail.com>
2026-03-12 11:23:16 +0100

    dd

--- public/js/map.js
+++ public/js/map.js
@@ -4,6 +4,6 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
 maxZoom:19
 }).addTo(map);
 
-const marker = L.marker([45.83,1.26]).addTo(map);
+const marker = L.marker([45.83547,1.2645]).addTo(map);
 
 marker.bindPopup("Ruche 1");
