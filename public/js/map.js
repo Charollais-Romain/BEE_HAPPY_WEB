5c0b7842e56da8629d36a5a7d351ea7613e1a13a
liloumorgan13-glitch <liloumorgan.13@gmail.com>
2026-03-13 13:45:12 +0100

    Update map.js

--- public/js/map.js
+++ public/js/map.js
@@ -1,9 +1,18 @@
 const map = L.map('map').setView([45.83, 1.26], 13);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-maxZoom:19
+  maxZoom:19
 }).addTo(map);
 
-const marker = L.marker([45.83547,1.2645]).addTo(map);
+const hives = [
+  {name:"Ruche 1", lat:45.83547, lng:1.2645},
+  {name:"Ruche 2", lat:45.83210, lng:1.2700},
+  {name:"Ruche 3", lat:45.82850, lng:1.2580},
+  {name:"Ruche 4", lat:45.84000, lng:1.2500}
+];
 
-marker.bindPopup("Ruche 1");
+hives.forEach(hive => {
+  L.marker([hive.lat, hive.lng])
+    .addTo(map)
+    .bindPopup(hive.name);
+});
