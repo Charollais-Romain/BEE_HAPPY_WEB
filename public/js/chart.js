879441b17c5b2c2a4c4d97c234068e3adb0dd9a5
liloumorgan13-glitch <liloumorgan.13@gmail.com>
2026-03-20 15:36:03 +0100

    upgrades

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,17 +1,39 @@
 const ctx = document.getElementById("temperatureChart").getContext("2d");
 
 const chart = new Chart(ctx, {
-type: "line",
-data: {
-labels: [],
-datasets: [{
-label: "Température ruche",
-data: [],
-borderWidth: 2
-}]
-},
-options: {
-responsive: true,
-maintainAspectRatio: false,
-}
+    type: "line",
+    data: {
+        labels: [],
+        datasets: [{
+            label: "Température ruche",
+            data: [],
+            borderWidth: 2,
+            tension: 0.3 // smooth curve
+        }]
+    },
+    options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        animation: false, // smoother real-time updates
+        scales: {
+            y: {
+                beginAtZero: false
+            }
+        }
+    }
 });
+
+function updateChart(temperature, hiveId) {
+    chart.data.labels.push(new Date().toLocaleTimeString());
+    chart.data.datasets[0].data.push(temperature);
+
+    // Update label dynamically
+    chart.data.datasets[0].label = `Température ${hiveId}`;
+
+    if (chart.data.labels.length > 20) {
+        chart.data.labels.shift();
+        chart.data.datasets[0].data.shift();
+    }
+
+    chart.update();
+}
--- public/js/socket.js
+++ public/js/socket.js
@@ -1,9 +1,7 @@
-// const socket = io();
-
-// socket.on("nouvelleDonnee", (data) => {
-
-// console.log(data);
 const socket = io();
+
+const markers = {};        // store markers per hive
+let selectedHive = null;   // which hive is shown in chart
 
 socket.on("connect", () => {
     console.log("Connecté au serveur");
@@ -12,25 +10,29 @@ socket.on("connect", () => {
 socket.on("nouvelleDonnee", (data) => {
     console.log("Donnée reçue :", data);
 
-    // envoyer les données au graphique
-    updateChart(data);
+    const { hiveId, temperature, humidite, lat, lng } = data;
+
+    // creates a marker if it doesn't exist
+    if (!markers[hiveId]) {
+        const marker = L.marker([lat, lng]).addTo(map);
+
+        marker.on("click", () => {
+            selectedHive = hiveId;
+            console.log("Ruche sélectionnée :", hiveId);
+        });
+
+        markers[hiveId] = marker;
+    }
+
+    // update le popup
+    markers[hiveId].setPopupContent(
+        `<b>${hiveId}</b><br>
+        Température : ${temperature}°C<br>
+        Humidité : ${humidite}%`
+    );
+
+    // Updates the chart only for selected hive
+    if (selectedHive === hiveId) {
+        updateChart(temperature, hiveId);
+    }
 });
-
-// mise à jour graphique
-chart.data.labels.push(new Date().toLocaleTimeString());
-chart.data.datasets[0].data.push(data.temperature);
-
-if(chart.data.labels.length > 20){
-chart.data.labels.shift();
-chart.data.datasets[0].data.shift();
-}
-
-chart.update();
-
-
-// mise à jour popup ruche
-marker.setPopupContent(
-"Température : " + data.temperature + "°C <br>" +
-"Humidité : " + data.humidite + "%"
-);
-
